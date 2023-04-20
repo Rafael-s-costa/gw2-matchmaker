7b6cb1ef131d8dff3edde67103b8cf390a21e650
Rafael-s-costa <rafael.s.costa96@gmail.com>
2023-04-20 19:25:37 +0100

    Matchmaking logic for support dps and dps.

--- src/main/java/com/rafaelscosta/gw2matchmaker/algorithm/MatchmakingAlgorithm.java
+++ src/main/java/com/rafaelscosta/gw2matchmaker/algorithm/MatchmakingAlgorithm.java
@@ -19,35 +19,20 @@ public class MatchmakingAlgorithm implements IMatchmakingAlgorithm {
      */
     @Override
     public void matchMake(List<Player> players) {
-        Map<String, List<Player>> playersByRole = new HashMap<>();
+        List<Player> playersClone = new ArrayList<>(players);
         List<Group> groups = new ArrayList<>();
 
         if (players.size() < 10) {
             throw new IllegalArgumentException("Not enough players to matchmake.");
         }
 
-        setPlayersByRole(players, playersByRole);
+        fillHealers(playersClone);
+        findQuicknessAlacrityPairs(playersClone);
+        fillDps(playersClone);
 
-        fillHealers(players);
-        findQuicknessAlacrityPairs(players);
-        fillDps(players);
+        removeGroupsNotFull(groups);
 
         removeGroupedPlayers(players, groups);
-    }
-
-    /*
-        Collects all players into a map, separating by each preferred role.
-     */
-    private void setPlayersByRole(List<Player> players, Map<String, List<Player>> playersByRole) {
-        for (Player player : players) {
-            for (EnumPlayerRole role : player.getSelectedRoles()) {
-                if (!playersByRole.containsKey(role.toString())) {
-                    playersByRole.put(role.toString(), new ArrayList<>());
-                }
-
-                playersByRole.get(role.toString()).add(player);
-            }
-        }
     }
 
     /*
@@ -69,21 +54,79 @@ public class MatchmakingAlgorithm implements IMatchmakingAlgorithm {
             group.setPlayers((HashMap<String, Player>) Map.of("healer_1", healer1, "healer_2", healer2));
             group.setActivityName("Healers");
             groups.add(group);
+
+            players.remove(healer1);
+            players.remove(healer2);
         }
     }
 
     /*
-        Find pairs of opposite support type from the healers, for each team.
+        Find pairs of opposite dps support type from the healers, for each team.
      */
     private void findQuicknessAlacrityPairs(List<Player> players) {
+        for (Group group: groups) {
+            Player healer1 = group.getPlayers().get("healer_1");
+            Player healer2 = group.getPlayers().get("healer_2");
 
+            List<Player> supportDpsList = players.stream()
+                    .filter(player -> player.getSelectedRoles().containsAll(Arrays.asList(EnumPlayerRole.QUICKNESS_DPS, EnumPlayerRole.ALACRITY_DPS)))
+                    .toList();
+
+            for (int i = 0; i < supportDpsList.size(); i = i + 2) {
+                Player dps1 = supportDpsList.get(i);
+                Player dps2 = supportDpsList.get(i + 1);
+
+                if (healer1.getCurrentRole().equals(EnumPlayerRole.QUICKNESS_HEALER) && healer2.getCurrentRole().equals(EnumPlayerRole.QUICKNESS_HEALER)) {
+                    dps1.setCurrentRole(EnumPlayerRole.ALACRITY_DPS);
+                    dps2.setCurrentRole(EnumPlayerRole.ALACRITY_DPS);
+                } else if (healer1.getCurrentRole().equals(EnumPlayerRole.ALACRITY_HEALER) && healer2.getCurrentRole().equals(EnumPlayerRole.ALACRITY_HEALER)) {
+                    dps1.setCurrentRole(EnumPlayerRole.QUICKNESS_DPS);
+                    dps2.setCurrentRole(EnumPlayerRole.QUICKNESS_DPS);
+                } else {
+                    dps1.setCurrentRole(EnumPlayerRole.QUICKNESS_DPS);
+                    dps2.setCurrentRole(EnumPlayerRole.ALACRITY_DPS);
+                }
+
+                group.getPlayers().put("support_dps_1", dps1);
+                group.getPlayers().put("support_dps_2", dps2);
+
+                players.remove(dps1);
+                players.remove(dps2);
+            }
+        }
     }
 
     /*
         Fill the teams with the rest of the players, DPS.
      */
     private void fillDps(List<Player> players) {
-        Queue<Player> dpsQueue = new LinkedList<>(players);
+        List<Player> dpsList = players.stream()
+                .filter(player -> player.getSelectedRoles().contains(EnumPlayerRole.DPS))
+                .toList();
+
+        for (Group group: groups) {
+            int batchSize = 6;
+            int numBatches = (dpsList.size() + batchSize - 1) / batchSize; // round up to the nearest integer
+            for (int batchIndex = 0; batchIndex < numBatches; batchIndex++) {
+                int fromIndex = batchIndex * batchSize;
+                int toIndex = Math.min(fromIndex + batchSize, dpsList.size());
+                List<Player> dpsBatch = dpsList.subList(fromIndex, toIndex);
+
+                if (dpsBatch.size() < 6) {
+                    continue;
+                }
+
+                for (int i = 0; i < dpsBatch.size(); i++) {
+                    Player dps = dpsBatch.get(i);
+                    group.getPlayers().put("dps_" + (i + 1), dps);
+                    players.remove(dps);
+                }
+            }
+        }
+    }
+
+    private void removeGroupsNotFull(List<Group> groups) {
+        groups.removeIf(group -> group.getPlayers().size() < 10);
     }
 
     /*
